451bd48171b90c36f54a581b56f8301597c5f6b0
sadhahacker <sadha30102001@example.com>
2025-01-01 04:07:58 +0530

    first commit

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        Hello World
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
