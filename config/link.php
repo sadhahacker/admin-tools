6b87875c61865ce720a8b05eb2f59928cc1170bd
sadhahacker <sadha30102001@example.com>
2025-01-01 12:22:03 +0530

    refactor path

--- config/link.php
+++ config/link.php
@@ -2,12 +2,12 @@
 
 return [
     'css' => [
-        'AdminLTE' => '/theams/adminlte/dist/css/adminlte.css',
+        'AdminLTE' => '/theams/adminlte/css/adminlte.css',
         'Bootstrap' => '/theams/bootstrap/css/bootstrap.css',
         'BootstrapIcons' => '/theams/bootstrapIcons/font/bootstrap-icons.css',
     ],
     'js' => [
-        'AdminLTE' => '/theams/adminlte/dist/js/adminlte.js',
+        'AdminLTE' => '/theams/adminlte/js/adminlte.js',
         'Bootstrap' => '/theams/bootstrap/js/bootstrap.js',
     ],
 ];
