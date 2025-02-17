9c18e04e12ea0b735b064b1e58877127ab56e9f3
sadhasivam <sadha30102001@gmail.com>
2025-02-17 21:56:05 +0530

    add phpinfo

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('index', [SampleTrading::class,'market']);
 
+Route::get('phpinfo', function () {
+    ob_start();
+    phpinfo();
+    $phpinfo = ob_get_clean();
+
+    return response($phpinfo)->header('Content-Type', 'text/html');
+});
+
+
 Route::post('execute', [TradingController::class,'executeTrade'])->withoutMiddleware(['web']);
